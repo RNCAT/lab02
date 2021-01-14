0a4c3af28f1eedde33edfe30f417ce9c46479d2d
RNCAT <xshootercast@gmail.com>
2021-01-14 12:28:01 +0700

    directives text, show, for, if, bind

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span v-text="message"></span>
+    <hr>
+    <h2 v-show="isShow">Hello</h2>
+    <hr>
+    <h2 :key="emp.id" v-for="emp in employee">
+      <span v-bind:class="emp.salary >= 90000 ? 'high' : emp.salary >= 30000 ? 'medium' : 'low'">
+        {{ emp.name }}
+      </span>
+      <span v-if="emp.salary >= 90000">***</span>
+      <span v-else-if="emp.salary >= 30000">**</span>
+      <span v-else>*</span>
+    </h2><br>
+    <a :href="urlSKRU">SKRU</a><br><br>
+    <img :src="require('@/assets/img/'+ imagesName + '.jpg')" alt="cat" width="100px" height="100px">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data(){
+    return {
+      imagesName: "cat", 
+      urlSKRU: "https://www.skru.ac.th",
+      message: "Hello World",
+      isShow: true,
+      employee: [
+        {"id":1, "name":"Cat", "salary":90000},
+        {"id":2, "name":"Dog", "salary":30000},
+        {"id":3, "name":"Fish", "salary":10000},
+      ],
+    }
+  },
 }
 </script>
 
@@ -24,5 +47,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.high {
+  color: green;
+}
+.medium {
+  color: orange;
+}
+.low {
+  color: red;
 }
 </style>
